039c89e5ef4c1d11ba46ff1f381ba7e9c4f35c29
mkreyman <mark@kreyman.com>
2017-12-06 10:32:40 -0700

    Fixed failing test

--- test/table_formatter_test.exs
+++ test/table_formatter_test.exs
@@ -36,14 +36,13 @@ defmodule TableFormatterTest do
       TF.print_table_for_columns(simple_test_data(), headers())
   end
 
-  assert result ==
-    """
-    c1 | c2 | c4
+  assert result == """
+    c1    | c2     | c4\s\s\s\s\s
     ------+--------+--------
-    r1 c1 | r1 c2 | r1+++c4
-    r2 c1 | r2 c2 | r2 c4
-    r3 c1 | r3 c2 | r3 c4
-    r4 c1 | r4++c2 | r4 c4
+    r1 c1 | r1 c2  | r1+++c4
+    r2 c1 | r2 c2  | r2 c4\s\s
+    r3 c1 | r3 c2  | r3 c4\s\s
+    r4 c1 | r4++c2 | r4 c4\s\s
     """
   end
 end
